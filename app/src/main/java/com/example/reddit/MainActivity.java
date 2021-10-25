ee50e5fac67b86151f10ef0c3e78584c7cdd8b3f
DenisChelaru <84143830+DenisChelaru@users.noreply.github.com>
2021-10-25 15:14:38 +0300

    Tema 2

---
+++ app/src/main/java/com/example/reddit/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.reddit;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignUp;
+    private Button btnForgotPass;
+    private final int mainActivityRequest=100;
+    private TextView username;
+    private TextView password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Log.v("lifecycle","onCreate");
+        Toast.makeText(MainActivity.this, "Logheaza-te",Toast.LENGTH_LONG).show();
+        btnSignUp=(findViewById(R.id.btn_Login));
+        username=findViewById(R.id.tb_Username);
+        password=findViewById(R.id.tb_Password);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Creeaza-ti un cont",Toast.LENGTH_LONG).show();
+                Intent newWindow= new Intent(MainActivity.this,MainActivity2.class);
+               // startActivity(newWindow);
+                startActivityForResult(newWindow,mainActivityRequest);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.v("lifecycle","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.v("lifecycle","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.v("lifecycle","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.v("lifecycle","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.v("lifecycle","onDestroy");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==mainActivityRequest)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                if(data!=null)
+                {
+                    Bundle newBundle=data.getBundleExtra("raspunsBundle");
+                    Utilizator u1= (Utilizator) newBundle.getSerializable("User");
+                    username.setText(u1.getUsername().toString());
+                    password.setText(u1.getPassword().toString());
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/reddit/Utilizator.java
@@ -0,0 +1,51 @@
+package com.example.reddit;
+
+import java.io.Serializable;
+
+public class Utilizator implements Serializable {
+    private String email;
+    private String username;
+    private String password;
+
+    public Utilizator(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Utilizator() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Utilizator{" +
+                "email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
